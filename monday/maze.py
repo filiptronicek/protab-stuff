901655428e27051752c58f3f7c9106d4db28bff1
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-26 13:03:54 +0200

    Add basic movement

--- monday/maze.py
+++ monday/maze.py
@@ -11,19 +11,21 @@ def find(char: str):
     for iy, line in enumerate(lines):
         for ix, c in enumerate(line):
             if char == c:
-                return [ix, iy]
+                return [iy, ix]
 
-def canMove(y, x): return not lines[y][x] == "#"
+def canMove(y, x): 
+    return not lines[y][x] == "#"
 
 positions = {
     "start": find("s"),
     "endpos": find("c"),
-    "currpos": find("s")
 }
+positions["currpos"] = positions["start"]
 
 def move():
     curr = positions["currpos"]
     endp = positions["endpos"]
+    
     def MvRight():
         curr = positions["currpos"]
         endp = positions["endpos"]
@@ -32,7 +34,9 @@ def move():
             positions["currpos"] = nextPos
             print("Moved to "+str(curr))
             return True
-        else: return False
+        else: 
+            return False
+
     def MvLeft():
         curr = positions["currpos"]
         endp = positions["endpos"]
@@ -41,17 +45,34 @@ def move():
             positions["currpos"] = nextPos
             print("Moved to "+str(curr))
             return True
-        else: return False
+        else: 
+            return False
+
     def MvDown():
         curr = positions["currpos"]
         endp = positions["endpos"]
-        nextPos = [curr[0]+1, curr[1]]
+        nextPos = [curr[0] + 1, curr[1]]
         if canMove(nextPos[0], nextPos[1]):
             positions["currpos"] = nextPos
             print("Moved to "+str(curr)) 
             return True
-        else: return False
+        else:
+            return False
+    def MvUp():
+        curr = positions["currpos"]
+        endp = positions["endpos"]
+        nextPos = [curr[0] - 1, curr[1]]
+        if canMove(nextPos[0], nextPos[1]):
+            positions["currpos"] = nextPos
+            print("Moved to "+str(curr)) 
+            return True
+        else:
+            return False
+    i = 0
     while positions["currpos"] != positions["endpos"]:
+        print(f"Curr pos: {str((positions['currpos']))}\nEnd: {str(positions['endpos'])}")
+        if not MvDown(): MvUp()
         if not MvRight(): MvLeft()
-        MvDown()
+    print(f"Curr pos: {str(positions['currpos'])}\nEnd: {str(positions['endpos'])}")
+
 move()
