1b83f0fa713d0b5429f250af8d19c7b3ca7bc86c
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-26 21:07:50 +0200

    Try through all combinations of keys

--- ulozky/pro_maze.py
+++ ulozky/pro_maze.py
@@ -13,29 +13,25 @@ moje_y = c.y()
 keyCombos = [(0, 1, "s"), (0, -1, "w"), (1, 0, "d"), (-1, 0, "a")]
 
 positions = {
-    "curr": [moje_x, moje_y],    
+    "curr": [moje_x, moje_y]    
 }
 
 def canMove(x, y, amm):
-    return field[y][x] == 0 or field[y][x] == 3 and [x,y] not in amm
+    return (field[y][x] == 0 or field[y][x] == 3) and ([x,y] not in amm and x > 0 and y > 0)
 
 def move(x, y, amm):
     if canMove(x,y, amm):
         positions["curr"] = [x, y]
         amm.append([x, y])
 def calcPath():
-    ammunation = [[moje_x, moje_y]] # You cannot make this sound cooler
+    ammunation = [[positions['curr'][0], positions['curr'][1]]] # You cannot make this sound cooler
     while True:
-        if(canMove(moje_x + 1, moje_y, ammunation)):
-            move(moje_x + 1, moje_y, ammunation)
-        elif(canMove(moje_x - 1, moje_y, ammunation)):
-            move(moje_x - 1, moje_y, ammunation)
-        elif(canMove(moje_x, moje_y + 1, ammunation)):
-            move(moje_x, moje_y + 1, ammunation)
-        elif(canMove(moje_x, moje_y - 1, ammunation)):
-            move(moje_x, moje_y - 1, ammunation)
-        else:
+        for moveN in keyCombos:
+            if canMove(positions['curr'][0] + moveN[0], positions['curr'][1] + moveN[1], ammunation):
+                move(moveN[2])
+                break
+        if(positions['curr'] == ammunation[-1]):
+            print(ammunation)
             return
         print(positions['curr'])
-
 calcPath()
