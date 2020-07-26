cb04c188db4b757d71934b2c3e04a744d617bc0d
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-26 09:11:20 +0000

    Add tuple list script

--- monday/tuple_list.py
+++ monday/tuple_list.py
@@ -1,5 +1,11 @@
+import sys
+
 y = [x for x in range(10) if x % 3 == 0]
 yt = {x for x in range(10) if x % 3 == 0}
 
 print(y)
 print(yt)
+
+print({2**i: i for i in range(40) if 2**i < 9223372036854775807})
+
+print(sys.maxsize)
