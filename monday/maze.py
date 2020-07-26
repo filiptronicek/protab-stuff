c64e3cc5ee41569241fcb9d9f913264ae0ecf3d9
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-26 10:27:51 +0000

    try moving left, right

--- monday/maze.py
+++ monday/maze.py
@@ -14,11 +14,32 @@ def find(char: str):
                 return [ix, iy]
 
 def canMove(x, y):
-    return not lines[y][x] == "#"
+ return not lines[y][x] == "#"
 
 positions = {
     "start": find("s"),
     "endpos": find("c"),
     "currpos": find("s")
 }
-print(positions)
+
+def move():
+    curr = positions["currpos"]
+    endp = positions["endpos"]
+    def MvRight():
+        curr = positions["currpos"]
+        endp = positions["endpos"]
+        nextPos = [curr[0], curr[1] + 1]
+        if canMove(nextPos[0], nextPos[1]): 
+            curr = nextPos
+            print("Moved to "+curr)
+    def MvLeft():
+        curr = positions["currpos"]
+        endp = positions["endpos"]
+        nextPos = [curr[0], curr[1] - 1]
+        if canMove(nextPos[0], nextPos[1]): 
+            curr = nextPos
+            print("Moved to "+curr) 
+    while positions["currpos"] != positions["endpos"]:
+        MvRight()
+        MvLeft()
+move()
