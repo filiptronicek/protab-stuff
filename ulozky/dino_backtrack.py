37b4c61c49519d5dddbb779deaa1ac16a3832c34
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-27 23:02:48 +0200

    Print all the coordinates, but wrongfully

--- ulozky/dino_backtrack.py
+++ ulozky/dino_backtrack.py
@@ -9,8 +9,8 @@ field = c.get_all()
 jmpLen = 4
 
 inp = []
-comms = [10000]
-now = comms[0]
+comms = [[10000, 0]]
+now = comms[0][0]
 for line in field:
     inp.append(line[-1])
 print("Aiming to hit:",len(inp))
@@ -25,23 +25,33 @@ r = list(reversed(inp))
 for g in range(len(inp)):
     if now < 0: break
     for i in range(jmpLen):
-        print("Counting for",now)
-        lastNodeIndex = now - jmpLen - i
+        lastNodeIndex = now - jmpLen + i
         lastNode = inp[lastNodeIndex] 
 
-        leftNodeIndex = now - jmpLen - 1 - i
+        leftNodeIndex = now - jmpLen - 1 + i
         leftNode = inp[leftNodeIndex]
+
         if leftNode == 1:
             now = leftNodeIndex
+            comms.append([leftNodeIndex, 0])
+
+            break
         else:
             if lastNode == 1:
                 now = lastNodeIndex
-                comms.append(now - jmpLen - i)
+                comms.append([lastNodeIndex, 1])
                 break
+            else:
+                comms.append([lastNodeIndex, 0])
+
 print(comms)
-for p in range(len(inp)):
-    time.sleep(0.5)
-    if p in comms:
+c.wait()
+"""
+while True:
+    print(f"If {c.x()} in comms")
+    if c.x() in comms:
+        print("Yes")
         jump()
     else:
         move()
+"""
